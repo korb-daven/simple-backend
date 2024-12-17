47cae594254cd01f8d35658d7433f5cdaf31204a
Korb Daven <korbdaven@gmail.com>
2024-12-17 21:57:24 +0700

    update code

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,12 +5,22 @@ import { CategoriesModule } from './category/category.module';
 import { SequelizeModule } from '@nestjs/sequelize';
 import { dataBaseConfig } from './database/database.config';
 import { PromotionsModule } from './promotion/promotion.module';
+import { GroupsModule } from './group/group.module';
+import { ProductsModule } from './product/product.module';
+import { ServeStaticModule } from '@nestjs/serve-static';
+import { join } from 'path';
 
 @Module({
   imports: [
     SequelizeModule.forRoot(dataBaseConfig),
     CategoriesModule,
     PromotionsModule,
+    GroupsModule,
+    ProductsModule,
+    ServeStaticModule.forRoot({
+      rootPath: join(__dirname, '..', 'uploads'), // Serve from the uploads folder
+      serveRoot: '/uploads', // Serve files at http://localhost:3000/uploads
+    }),
   ],
   controllers: [AppController],
   providers: [AppService],
